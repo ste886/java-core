6a0d74120d5d0a0d36416ddc421fb39e6ad604da
Stefania Caputo <stefania.caputo.cv@gmail.com>
2023-06-22 20:55:16 +0200

    Even Odd

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+public class EvenOdd {
+	public static void main(String[] args) {
+		int x = Integer.parseInt(args[0]);
+		if (x % 2 == 0) {
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+				
+			
+	}
+}
